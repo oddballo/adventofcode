534772c78e8372f696fe709f80ffdcc6974e0da4
Owen Davies <owen@stwcreation.com>
2022-12-07 19:13:12 +0000

    Improvements

--- challenge7/challenge.sh
+++ challenge7/challenge.sh
@@ -4,27 +4,19 @@ files(){
     FILENAME=$1
     CURRENT=""
     while read -r ENTRY; do
-
-        PART=$(echo "$ENTRY" | cut -f 1 -d" ")
-        if [[ "$PART" == "$" ]]; then
-            PART2=$(echo "$ENTRY" | cut -f 2 -d" ")
-            if [ "$PART2" == "cd" ]; then
-                PART3=$(echo "$ENTRY" | cut -f 3 -d" ")
-                if [[ "$PART3" == ".." ]]; then
-                    CURRENT="$(echo "$CURRENT" | rev | cut -f2- -d"/" | rev)"
-                elif [ -z "$CURRENT" ]; then
-                    if [[ "$PART3" != "/" ]]; then
-                        CURRENT="/$PART3"
-                    fi
-                elif [[ "$CURRENT" == "/" ]]; then
-                    CURRENT="$CURRENT$PART3"
-                else
-                    CURRENT="$CURRENT/$PART3"
-                fi
+        IFS=" " read PART1 PART2 PART3 <<< "$ENTRY" 
+        if [[ "$PART1" == "$" ]] && [[ "$PART2" == "cd" ]]; then
+            if [[ "$PART3" == ".." ]]; then
+                CURRENT="$(echo "$CURRENT" | rev | cut -f2- -d"/" | rev)"
+            elif [[ "$PART3" == "/" ]]; then
+                CURRENT="/"
+            elif [[ "$CURRENT" == "/" ]]; then
+                CURRENT="$CURRENT$PART3"
+            else
+                CURRENT="$CURRENT/$PART3"
             fi
-        elif [[ "$PART" != "dir" ]]; then
-            FILENAME="$(echo "$ENTRY" | cut -f 2 -d" ")"
-            echo "$CURRENT/$FILENAME,$PART"
+        elif [ "$PART1" ] && [ -z "${PART1//[0-9]}" ]; then
+            echo "$CURRENT/$PART2,$PART1"
         fi
     done < "$FILENAME"
 }
@@ -33,20 +25,14 @@ totalFolders(){
     local FILES=("$@")
     declare -A FOLDER
     for FILE in "${FILES[@]}"; do
-        FILEPATH="$(echo "$FILE" | cut -f1 -d",")"
-        SIZE="$(echo "$FILE" | cut -f2 -d",")"
-        CURRENT=""
-
+        IFS="," read FILEPATH SIZE <<< "$FILE" 
         IFS='/' read -a PARTS <<< "$FILEPATH"
+        CURRENT=""
         LENGTH=${#PARTS[@]}
         # Stop one short on the folder; we don't care for files
         for (( j=0; j < LENGTH - 1; j++ )); do
             PART=${PARTS[$j]}
-            if [ -z "$PART" ]; then
-                CURRENT="/"
-            else
-                CURRENT="$CURRENT$PART/"
-            fi
+            CURRENT="$CURRENT$PART/"
             if [ ! "${FOLDER["$CURRENT"]+a}" ]; then
                 FOLDER["$CURRENT"]=0
             fi
